723f5be426fb5c510b386e0c2a841cf4e4c3a211
takajin1989 <ya65857@ubuntu.ubuntu-domain>
2016-11-22 02:10:10 +0900

    commit

--- question5.py
+++ question5.py
@@ -1,103 +1,92 @@
 # -*- coding: utf-8 -*-
-# 未完成
-# 二次関数の近似を行いたい
-# y_sample = 0.1*x_sample*x_sample + 0.3
-# 演習のためテンソルを増やしてみる
-# 入力層１個、隠れ層(4*4)*2個,出力層１個のモデルを作成せよ
-# それぞれ重みWとバイアスbを使うこと
+# ４次関数の近似を行う
+# ただし、５次以上の関数についても近似できるように
+# 一般化してコードを書く
 
 import tensorflow as tf
 import numpy as np
 import time
+import random
+import math
 import matplotlib.pyplot as plt
 
 start = time.time()
+AC=100
+# number of the function s dimention
+NFD = 4
+#1+number of the function s dimention
+NN=NFD+1
+# number of W
+WN=NN
 
-# data set
-x_sample = np.random.rand(100,1).astype("float32")
-y_sample = 0.1*x_sample*x_sample*x_sample + 0.3
-y_sample = y_sample
+a = np.arange(0.1,(WN+1)*0.1,0.1)
 
-x_data = tf.placeholder(tf.float32,[1])
-y_data = tf.placeholder(tf.float32,[1])
+x_data = np.random.rand(AC,NN,1).astype("float32")
+y_data = np.zeros((AC,NN,1)).astype("float32")
+npow = 1
 
-w_input = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b_input = tf.Variable(tf.zeros([1]))
+for i in xrange(NN):
+    y_data += a[i] * npow
+    npow *= x_data
 
-w = tf.Variable(tf.random_uniform([4,4], -1.0, 1.0))
-b = tf.Variable(tf.zeros([4,4]))
+W = tf.Variable(tf.random_uniform([WN], -1.0, 1.0))
+y = 0
+npow = 1
 
-w2 = tf.Variable(tf.random_uniform([4,4], -1.0, 1.0), "w2")
-b2 = tf.Variable(tf.zeros([4,4]), "b2")
 
-w_output = tf.Variable(tf.random_uniform([4,4], -1.0, 1.0))
-b_output = tf.Variable(tf.zeros([1]))
-
-# input layer
-h_linear1 = tf.add(w_input*x_data*x_data, b_input)
-
-# first hidden layer
-h_linear2 = tf.add(h_linear1*w, b)
-
-# first hidden layer
-h_linear3 = tf.matmul(h_linear2, w2) + b2
-
-# output layer
-h_linear_output = tf.reduce_sum(tf.matmul(h_linear2,w_output)) + b_output
-
-y = h_linear_output
-
-# Add summary ops to collect data
-w_hist = tf.histogram_summary("weights2", w2)
-b_hist = tf.histogram_summary("biases2", b2)
-y_hist = tf.histogram_summary("y", y)
+for i in xrange(WN):
+    y += W[i] * npow
+    npow *= x_data
 
 loss = tf.reduce_mean(tf.square(y_data - y))
-loss_summary = tf.scalar_summary("loss", loss)
-
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
 init = tf.initialize_all_variables()
 
 sess = tf.Session()
-merged = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter("/tmp/tensorflow_log", sess.graph_def)
 sess.run(init)
 
-for step in xrange(10001):
-    for i in xrange(100):
-        if step % 100 == 0:
-            result = sess.run([merged, loss],feed_dict={x_data:x_sample[i], y_data:y_sample[i]})
-            summary_str = result[0]
-            acc = result[1]
-            writer.add_summary(summary_str, step)
-        else:
-            sess.run(train, feed_dict={x_data:x_sample[i], y_data:y_sample[i]})
-    if step % 100 == 0:
-        print step
-        print sess.run(w2)
-        print sess.run(b2)
+for step in xrange(1000*pow(2,NN+2)):
+    sess.run(train)
+    if step % (10*pow(2,NN)) == 0:
+        print "training...", sess.run(W)
 
-xx = np.arange(-10, 10, 1)
-yy = 0.1 * xx * xx* xx + 0.3
+plot_x = []
+plot_y = []
 
-plt.plot(xx, yy)
+for i in xrange(100):
+    result = 0
+    npow = 1
+    xx = i-50
+    for j in xrange(WN):
+        result += W[j] * npow
+        npow *= xx
+    plot_x.append(xx)
+    plot_y.append(sess.run(result))
+    # print i, sess.run(result)
 
-x=[]
-r = []
+x_correct = []
+y_correct = []
 
-for i in xrange(20):
-    prot_x = i-10
-    x.append(prot_x)
-    r.append(sess.run(y, feed_dict={x_data:[prot_x]}))
-    print(r[i])
+for i in xrange(100):
+    result = 0
+    npow = 1
+    xx = i-50
+    for j in xrange(NN):
+        result += a[j] * npow
+        npow *= xx
+    x_correct.append(xx)
+    y_correct.append(result)
+    # print i, result
 
-plt.scatter(x, r)
+plt.plot(x_correct, y_correct)
+plt.scatter(plot_x, plot_y)
 
 plt.show()
 
 sess.close()
 
 timer = time.time() - start
-print(("time:{0}".format(timer)) + "[sec]")
+
+print ("time:{0}".format(timer)) + "[sec]"
